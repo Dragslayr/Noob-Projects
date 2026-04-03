e2c9cc89e04baf93551c78d411aca0df87d4de2e
Parnava Samanta <parnavasamanta@gmil.com>
2026-04-03 17:03:39 +0530

    added tic tac toe game

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,60 @@
+let cells = document.querySelectorAll(".cell");
+let resetButton = document.getElementById("reset-button");
+let cnt = 0;
+
+function markCell(event) {
+  let cell = event.target;
+  if (cell.textContent === "") {
+    if (cnt % 2 === 0) {
+      cell.textContent = "X";
+    } else {
+      cell.textContent = "O";
+    }
+    cnt++;
+  }
+  let winner = checkWin();
+  if (winner) {
+    alert(winner + " wins!");
+    resetGame();
+  } else if (cnt === 9) {
+    alert("It's a draw!");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  cells.forEach((cell) => {
+    cell.textContent = "";
+  });
+  cnt = 0;
+}
+
+function checkWin() {
+  let winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let pattern of winPatterns) {
+    let [a, b, c] = pattern;
+    if (
+      cells[a].textContent &&
+      cells[a].textContent === cells[b].textContent &&
+      cells[a].textContent === cells[c].textContent
+    ) {
+      return cells[a].textContent;
+    }
+  }
+  return null;
+}
+
+resetButton.addEventListener("click", resetGame);
+cells.forEach((cell) => {
+  cell.addEventListener("click", markCell);
+});
